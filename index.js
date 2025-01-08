6cf579a306698f6151f985fb6e57cf3d29b00122
Harshita222 <kumariharsita501@gmail.com>
2025-01-08 23:06:57 +0530

    ok

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 // middleware enable
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://mern-ecommerce-frontend-gray.vercel.app",
     methods: "GET,PUT,POST,DELETE",
   })
 );
